bd89a9eadc02bf655b6a8d7033cb2ec9948dd3e1
QQQEB159 <2724758997@qq.com>
2026-06-21 17:47:13 +0800

    6

--- source/funkin/states/substates/CosmicubeSubState.hx
+++ source/funkin/states/substates/CosmicubeSubState.hx
@@ -446,7 +446,7 @@ class CosmicubeSubState extends MusicBeatSubstate
 			CosmicubeData.setMoney(node.meta.currency, CosmicubeData.getMoney(node.meta.currency) - node.price);
 			
 			ClientPrefs.cosmicubeUnlocks.push(node.id);
-			FlxG.sound.play(Paths.sound('shopBuy'));
+			FlxG.sound.play(Paths.sound('shopbuy'));
 			node.unlocked = true;
 			checkCosmiCollectorAward();
 			checkTheHundredAward();
