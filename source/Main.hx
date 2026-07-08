f047e2815f186ba86b49cbf6120d46aa629f31bc
QQQEB159 <2724758997@qq.com>
2026-07-08 13:05:35 +0800

    huh

--- source/Main.hx
+++ source/Main.hx
@@ -48,10 +48,8 @@ class Main extends Sprite
 	public function new()
 	{
 		#if mobile
-		#if android
-		StorageUtil.requestPermissions();
-		#end
 		Sys.setCwd(StorageUtil.getStorageDirectory());
+		if (!FileSystem.exists('content/')) FileSystem.createDirectory('content/');
 		CoolUtil.deleteFolder('assets');
 		StorageUtil.copyNecessaryFiles();
 		#end
